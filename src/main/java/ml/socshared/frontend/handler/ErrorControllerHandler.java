c675d646ec39cdbd64a197b600a4eb2a86090ccf
Владислав Овчинников <vladovchinnikov950@gmail.com>
2020-06-04 17:27:29 +0300

    Поправил обработку ошибок

--- src/main/java/ml/socshared/frontend/handler/ErrorControllerHandler.java
+++ src/main/java/ml/socshared/frontend/handler/ErrorControllerHandler.java
@@ -1,6 +1,8 @@
 package ml.socshared.frontend.handler;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import ml.socshared.frontend.exception.AbstractRestHandleableException;
 import ml.socshared.frontend.exception.impl.HttpForbiddenException;
 import ml.socshared.frontend.exception.impl.HttpNotFoundException;
 import ml.socshared.frontend.exception.impl.HttpUnauthorizedException;
@@ -18,21 +20,25 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @ControllerAdvice
+@Slf4j
 public class ErrorControllerHandler {
 
     @ExceptionHandler({HttpForbiddenException.class, HttpNotFoundException.class})
-    public String forbidden() {
+    public String forbidden(AbstractRestHandleableException exception) {
+        log.error("{}: {}", exception.getHttpStatus(), exception.getMessage());
         return "404";
     }
 
     @ExceptionHandler(HttpUnauthorizedException.class)
     public String unauthorized(HttpUnauthorizedException exception) {
-        return "redirect:/";
+        log.error("{}: {}", exception.getHttpStatus(), exception.getMessage());
+        return "landing_page";
     }
 
     @ExceptionHandler(Exception.class)
     public String otherException(Exception exc) {
-     //   exc.printStackTrace();
+        exc.printStackTrace();
+        log.error("{}: {}", HttpStatus.INTERNAL_SERVER_ERROR, exc);
         return "500";
     }
 }

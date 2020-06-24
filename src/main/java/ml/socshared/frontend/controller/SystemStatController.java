a2342a78fd39db7bb26aae12e1860a41f431d1f6
Владислав Овчинников <vladovchinnikov950@gmail.com>
2020-06-24 20:55:21 +0300

    Полностью доработана страница статистики по социальным сетям

--- src/main/java/ml/socshared/frontend/controller/SystemStatController.java
+++ src/main/java/ml/socshared/frontend/controller/SystemStatController.java
@@ -35,22 +35,50 @@ public class SystemStatController {
     }
 
     @GetMapping("/sys_stat/soc_network")
-    public String getSocStat(@ModelAttribute("total_count") TotalStatsResponse totalCount,
-                             @CookieValue(name = "JWT_AT", defaultValue = "") String accessToken, Model model) {
+    public String getSocStat(@CookieValue(name = "JWT_AT", defaultValue = "") String accessToken, Model model) {
 
         UsingSocialNetworkResponse usingSocialNetworkResponse = statService.getUsingSocialNetworkStat(accessToken);
+        TotalStatsResponse totalStatsResponse = statService.getTotalCount(accessToken);
 
         model.addAttribute("using_social_network", usingSocialNetworkResponse);
-        model.addAttribute("total_count", totalCount);
+        model.addAttribute("total_count", totalStatsResponse);
 
         model.addAttribute("bread", new Breadcrumbs(Collections.emptyList(), "Системная статистика"));
 
-        PieChartData<Long, String> pieChartData = new PieChartData<>(
+        PieChartData<Long, String> usingFacebookVkChartData = new PieChartData<>(
                 Arrays.asList(usingSocialNetworkResponse.getVk().getAllEventsCount(),
                         usingSocialNetworkResponse.getFacebook().getAllEventsCount()),
                 Arrays.asList("Вконтакте", "Facebook"), Arrays.asList("#4d7198", "#3b5998")
         );
-        model.addAttribute("soc_pie_chart", pieChartData);
+        model.addAttribute("using_facebook_vk_chart_data", usingFacebookVkChartData);
+
+        PieChartData<Long, String> usingFacebookChartData = new PieChartData<>(
+                Arrays.asList(usingSocialNetworkResponse.getFacebook().getAddPostEventsCount(),
+                        usingSocialNetworkResponse.getFacebook().getPostByIdEventsCount(),
+                        usingSocialNetworkResponse.getFacebook().getPostsEventsCount(),
+                        usingSocialNetworkResponse.getFacebook().getRemoveAccountEventsCount(),
+                        usingSocialNetworkResponse.getFacebook().getSaveAccountEventsCount(),
+                        usingSocialNetworkResponse.getFacebook().getUserGroupEventsCount(),
+                        usingSocialNetworkResponse.getFacebook().getUserGroupsEventsCount(),
+                        usingSocialNetworkResponse.getFacebook().getUserInfoEventsCount()),
+                Arrays.asList("Публикация поста", "Получение поста по ID", "Получение списка постов", "Отключение аккаунта",
+                        "Подключение аккаунта", "Получение группы", "Получение групп", "Получение информации об аккаунте"),
+                Arrays.asList("#B22222", "#FFFF00", "#FFFFE0", "#EE82EE", "#808080", "#000080", "#008000", "#2F4F4F")
+        );
+        model.addAttribute("using_facebook_chart_data", usingFacebookChartData);
+
+        PieChartData<Long, String> usingVkChartData = new PieChartData<>(
+                Arrays.asList(usingSocialNetworkResponse.getVk().getAddPostEventsCount(),
+                        usingSocialNetworkResponse.getVk().getPostByIdEventsCount(),
+                        usingSocialNetworkResponse.getVk().getPostsEventsCount(),
+                        usingSocialNetworkResponse.getVk().getUserGroupEventsCount(),
+                        usingSocialNetworkResponse.getVk().getUserGroupsEventsCount(),
+                        usingSocialNetworkResponse.getVk().getUserInfoEventsCount()),
+                Arrays.asList("Публикация поста", "Получение поста по ID", "Получение списка постов", "Получение группы",
+                        "Получение групп", "Получение информации об аккаунте"),
+                Arrays.asList("#B22222", "#FFFF00", "#FFFFE0", "#EE82EE", "#808080", "#000080")
+        );
+        model.addAttribute("using_vk_chart_data", usingVkChartData);
 
         return "sys_soc_stat";
     }

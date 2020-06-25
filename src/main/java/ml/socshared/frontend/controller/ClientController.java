cd04355b068ab1405298603bb275ebfcfdb4e617
Владислав Овчинников <vladovchinnikov950@gmail.com>
2020-06-25 07:06:17 +0300

    Фикс clients

--- src/main/java/ml/socshared/frontend/controller/ClientController.java
+++ src/main/java/ml/socshared/frontend/controller/ClientController.java
@@ -73,7 +73,7 @@ public class ClientController {
         else
             clientService.updateClient(newClientRequest.getClientId(), newClientRequest, accessToken);
 
-        return "redirect:/app";
+        return "clients :: content";
     }
 
 }
